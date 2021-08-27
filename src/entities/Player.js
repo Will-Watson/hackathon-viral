637971ff3982e48a43cc3b2449fc4eb37f1f3069
William Watson <williampwatson1991@gmail.com>
2021-08-27 18:37:46 -0400

    feat: soldier player and his legs move together

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -4,11 +4,32 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
   constructor(scene, x, y, spriteKey) {
     super(scene, x, y, spriteKey);
 
+    this.scene = scene;
+    this.scene.add.existing(this);
+    this.scene.physics.world.enable(this);
+    this.setCollideWorldBounds(true);
+
     // << INITIALIZE PLAYER ATTRIBUTES HERE >>
   }
 
+  updateMovement(cursors) {
+    if (cursors.left.isDown) {
+      this.setVelocityX(-360);
+    } else if (cursors.right.isDown) {
+      this.setVelocityX(360);
+    } else if (cursors.up.isDown) {
+      this.setVelocityY(-360);
+    } else if (cursors.down.isDown) {
+      this.setVelocityY(360);
+    } else {
+      this.setVelocityX(0);
+      this.setVelocityY(0);
+    }
+  }
+
   // Check which controller button is being pushed and execute movement & animation
-  update() {
+  update(cursors) {
     // << INSERT CODE HERE >>
+    this.updateMovement(cursors);
   }
 }
--- src/scenes/FgScene.js
+++ src/scenes/FgScene.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import Player from '../entities/Player';
 
 export default class FgScene extends Phaser.Scene {
   constructor() {
@@ -8,6 +9,22 @@ export default class FgScene extends Phaser.Scene {
   preload() {
     // Preload Sprites
     // << LOAD SPRITES HERE >>
+    this.load.spritesheet(
+      'idleLegs',
+      'assets/spritesheets/SoldierIdleFeet.png',
+      {
+        frameWidth: 230,
+        frameHeight: 210,
+      }
+    );
+    this.load.spritesheet(
+      'idleSoldier',
+      'assets/spritesheets/SoldierHandgunIdle.png',
+      {
+        frameWidth: 230,
+        frameHeight: 210,
+      }
+    );
     // Preload Sounds
     // << LOAD SOUNDS HERE >>
   }
@@ -15,6 +32,9 @@ export default class FgScene extends Phaser.Scene {
   create() {
     // Create game entities
     // << CREATE GAME ENTITIES HERE >>
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.legs = new Player(this, 20, 400, 'idleLegs').setScale(0.25);
+    this.player = new Player(this, 20, 400, 'idleSoldier').setScale(0.25);
     // Create sounds
     // << CREATE SOUNDS HERE >>
     // Create collisions for all entities
@@ -25,5 +45,7 @@ export default class FgScene extends Phaser.Scene {
   // delta: time elapsed (ms) since last update() call. 16.666 ms @ 60fps
   update(time, delta) {
     // << DO UPDATE LOGIC HERE >>
+    this.player.update(this.cursors);
+    this.legs.update(this.cursors);
   }
 }
