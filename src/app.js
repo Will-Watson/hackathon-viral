b6d1f38307b07c9f1df186aeb3fc01038303a14d
William Watson <williampwatson1991@gmail.com>
2021-09-01 02:56:40 -0400

    fix: random stuff still attempting to deploy

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 39076, function () {
   console.log(
     'Express server listening on port %d in %s mode',
     this.address().port,
