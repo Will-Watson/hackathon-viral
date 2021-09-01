d159398dbc8938791318f8af4f05f08a8d6eb07e
William Watson <williampwatson1991@gmail.com>
2021-09-01 17:05:56 -0400

    fix: hopeful

--- src/scenes/FgScene.js
+++ src/scenes/FgScene.js
@@ -544,7 +544,8 @@ export default class FgScene extends Phaser.Scene {
     this.landingPage.setInteractive({ useHandCursor: true });
     this.landingPage.on('pointerdown', () => {
       this.scene.remove('MainScene');
-      window.history.back();
+      window.location.href =
+        'https://will-watson.github.io/hackathon-viral/index.html';
     });
 
     this.restartText.setInteractive({ useHandCursor: true });
