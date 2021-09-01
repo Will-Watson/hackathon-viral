dffc4c4a5518ea7c5e41bfd79022e94179a76eeb
William Watson <williampwatson1991@gmail.com>
2021-09-01 13:42:36 -0400

    fix: still trying to get this deploy working.. changed webpack files and server index to match one of my older projects that was deployed to heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,35 @@
-'use strict';
-
+const isDev = process.env.NODE_ENV === 'development';
 const webpack = require('webpack');
-const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-
-  mode: 'development',
-
+  mode: isDev ? 'development' : 'production',
+  entry: [
+    '@babel/polyfill', // enables async-await
+    './public/index.js',
+  ],
   output: {
-    path: path.resolve(__dirname, 'public'),
-    publicPath: '/',
-    filename: 'bundle.js',
+    path: __dirname,
+    filename: './public/bundle.js',
   },
-
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devtool: 'source-map',
+  watchOptions: {
+    ignored: /node_modules/,
+  },
   module: {
     rules: [
       {
-        test: [/\.vert$/, /\.frag$/],
-        use: 'raw-loader',
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
       },
     ],
   },
-
   plugins: [
     new webpack.DefinePlugin({
-      CANVAS_RENDERER: JSON.stringify(true),
-      WEBGL_RENDERER: JSON.stringify(true),
+      'process.env.NODE_ENV': JSON.stringify('development'),
     }),
   ],
 };
